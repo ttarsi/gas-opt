ab5387a1777bc12034e2068c22c04c7b253caf15
Tyler Tarsi <tytarsi@gmail.com>
2022-01-23 21:16:48 -0500

    test: some initial tests on reads

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,11 +2,19 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "../Contract.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Contract c;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+      c = new Contract();
+    }
+
+    function testReadStorage() public {
+        c.readStorage();
+    }
+    function testReadMemory() public {
+        c.readMemory();
     }
 }
