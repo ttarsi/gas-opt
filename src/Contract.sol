2a09ca825bf1742bd2a5e22a8f768fb1efc2bac6
Tyler Tarsi <tytarsi@gmail.com>
2022-01-23 21:16:48 -0500

    test: some initial tests on reads

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-contract Contract {}
+contract Contract {
+  struct Data {
+    uint256 number;
+    address where;
+  }
+
+  Data public data;
+
+  constructor() {
+    data.number = 100;
+    data.where = address(this);
+  }
+
+  function readStorage() public view {
+    Data storage _data = data;
+    data.number;
+    data.where;
+  }
+
+  function readMemory() public view {
+    Data memory _data = data;
+    data.number;
+    data.where;
+  }
+}
